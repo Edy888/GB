ef93bcbe623bd6d5a78011ccdc90217105a74ecd
Edy888 <solde@mail.ru>
2023-10-10 20:38:55 +0300

    Add js

---
+++ Знакомство с веб-технологиями/S3_Знакомство с JS/code/main.js
@@ -0,0 +1,21 @@
+
+let a = 152;
+let b = 2;
+let summ1 = a + b;
+let summ2 = a*b;
+let summ3 = b/a;
+
+console.log(summ1, summ2, summ3);
+
+let name = "Aleftina";
+console.log("Привет, ${name}");
+
+
+let name_is = prompt('Your name?');
+console.log('Hi, ${name}!');
+
+function printMane(name){
+    console.log('Hi, ${name}');
+};
+
+printMane('Edy');
